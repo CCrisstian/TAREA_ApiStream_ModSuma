2290758e2718141120288cc1bc22a361ccbf1c78
CCrisstian <crisstiann.c@gmail.com>
2024-02-26 17:32:45 -0300

    Tarea terminada

--- src/Tarea_Main.java
+++ src/Tarea_Main.java
@@ -10,15 +10,16 @@ public class Tarea_Main {
         AtomicInteger aux = new AtomicInteger(0);
 
         Stream<Integer> Stream_1 = Stream.generate(aux::incrementAndGet).limit(100) /*Generar 100 elementos*/
-        .filter(u -> (u % 10 != 0));    /*Filtrar - Eliminar elementos divisibles por 10*/
-
+                                            .filter(u -> (u % 10 != 0));    /*Filtrar - Eliminar elementos divisibles por 10*/
 
 
         Stream<Double> Stream_2 = Stream_1.map(u -> u*1.0)  /*Convertir cada elemento 'int' en 'double'*/
-                .map(u -> u/2); /*Dividir cada elemento por 2*/
+                                            .map(u -> u/2); /*Dividir cada elemento por 2*/
+
 
         Optional<Double> resultado = Stream_2.reduce(Double::sum);  /*Sumar todos los elementos*/
 
         System.out.println("Resultado final: " + resultado.get());
     }
+
 }
